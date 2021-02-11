5b66ce4495a4a9618dd4f9048810d2fb5a2d9e37
dudenasv-cambridgeassessment <dudenas.v@cambridgeassessment.org.uk>
2021-02-11 22:39:27 +0200

    removing my IP

--- terraform/main/modules.tf
+++ terraform/main/modules.tf
@@ -17,7 +17,7 @@ module "aws_security_groups" {
   }
   name               = local.env_name
   vpc_id             = aws_default_vpc.default.id
-  ingress_cidr_block = ["85.206.68.114/32", "172.31.0.0/16"] #my home IP
+  ingress_cidr_block = ["YOUR_HOME_IP", "172.31.0.0/16"] #my home IP
 }
 
 #Creating AWS instances in this example in total 4 (2 green, 2 blue) and attaching security groups
@@ -40,7 +40,7 @@ module "aws_ec2" {
   vpc_id                                    = aws_default_vpc.default.id
   associate_public_ip_address               = true
   public_key                                = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCpkCFnnIX5DqU6HXSt/ht01aAMdmk3QP8o0qdpQml4ZZ6jgszKwGOJZLk1Tip2CmXByDZZC9juiepqoIWgLC0xRuacbXN/VRn9cOkyEbY+WjbICAELjstTc1cXbByM7kpGsIC4UzpvLVoPVVdN/Tpj7M8Rt456jSXng5q1vH8ECKL0byuimBo5mSCVYwjeNRfQFNBLxGnTIEDfuBsYHoHqEZPM7/pZzvbWtQPwnEm70Y1Ox3BtCyO31tg8OnfUFBcLnAKRKPJYxIz6a4kxNgx/GgIZn80T/pqCy5biCQe1poj9n3FNoFil0sKQXD9r2bcjzR3iHtJCFP8UwMcrgRWcBOBexar14d0OfClKdy2JzQFgNkSMfdLYvEDSWeOo8UXn4ah7SvVWSy/chni4oY7064AtHqeWJcHiXMtWBKMT+sWphjAxyCLknv0x+Yhi1Zms9DrAB5jZw7iOOqmfY/eJrpjwIeKZq0jYlKP+1P4rSQJk9bx+agS6QRb8lSIJObs= vilius@LTP-VILIDUDE" #public RSA key
-  
+
 }
 
 #Creating AWS ALB, note commented out blue arn therefor it will point now to Green EC2 instances group
